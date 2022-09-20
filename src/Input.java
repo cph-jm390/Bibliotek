6ba05853b55cb8c0ceab9b20bf7f5a2500e5cdfb
Emilio Lagunas <Cph-ec100@cphbusiness.dk>
2022-09-20 12:50:15 +0200

    Fixed små fejl

--- src/Input.java
+++ src/Input.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class Input {
 
     Scanner scan = new Scanner(System.in);
+
+    public static String getString(String changelanguage) {
+
+        return changelanguage;
+    }
+
     public int nySession(){
         System.out.println("angiv låner ID med tal");
         String input = scan.nextLine();
